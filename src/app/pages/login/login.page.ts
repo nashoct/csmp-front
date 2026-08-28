b0b6b8a8ae83a2947f03eb21d967fbc4a715b425
Ignacio de la Cruz <ignacio.delacruz@viewnext.com>
2026-08-28 23:28:34 +0200

    Refactor login page layout and styles to enhance user experience with a new design. Updated HTML structure, added password visibility toggle, and improved styling for better accessibility and responsiveness.

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonButton } from '@ionic/angular/standalone';
+import { Router, RouterLink } from '@angular/router';
+import { IonContent } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { LoginService } from '../../services/login.service'
 import { Login } from '../../models/login.model'
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonItem, IonLabel, IonButton]
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
+    standalone: true,
+    imports: [IonContent, CommonModule, FormsModule, RouterLink]
 })
 export class LoginPage implements OnInit {
   user: Login = new Login('', '');
+  showPassword = false;
 
   constructor(private router: Router, public loginService: LoginService) {}
 
